f2f9c046e3afdfaf0b14bcfb4eab7007d8c307da
2211142001 <330682339+2211142001@users.noreply.github.com>
2026-09-18 04:42:56 +0000

    feat: validationを実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+    validates :title, presence: true, length: { maximum: 50 }
+    validates :author, presence: true
+    validates :isbn, format: /(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+/, allow_blank: false
 end

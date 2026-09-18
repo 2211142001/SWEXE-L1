bc1d861a662940a714482be22ac8dc9cd90fa1d6
2211142001 <330682339+2211142001@users.noreply.github.com>
2026-09-18 04:43:09 +0000

    enhance: flashを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,14 +8,20 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(title: params[:book][:title], author: params[:book][:author], isbn: params[:book][:isbn])
-    book.save
-    redirect_to "/"
+    @book = Book.new(title: params[:book][:title], author: params[:book][:author], isbn: params[:book][:isbn])
+
+    if @book.save
+      flash[:notice] = "本を登録しました"
+      redirect_to @book
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "本を削除しました"
     redirect_to "/"
   end
 
@@ -28,8 +34,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(title: params[:book][:title], author: params[:book][:author], isbn: params[:book][:isbn])
-    redirect_to "/"
+    @book = Book.find(params[:id])
+    if @book.update(title: params[:book][:title], author: params[:book][:author], isbn: params[:book][:isbn])
+      flash[:notice] = "本を更新しました"
+      redirect_to @book
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 end
